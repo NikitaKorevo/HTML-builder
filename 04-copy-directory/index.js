6acb42b1fc4fb2dad80c13fffd66f4280659e388
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-11-07 13:20:49 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) throw err;
+
+  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+      if (err) return;
+    });
+    
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+});
