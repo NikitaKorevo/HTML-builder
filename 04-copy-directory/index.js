4d79106dfa5fe1cd241ba4c52f4987af71692931
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-11-07 18:51:00 +0300

    refactor: done from callback to async

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const fs = require('fs');
+const { readdir, rm, mkdir, copyFile } = fs.promises;
 
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-  if (err) throw err;
-
-  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
-    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-      if (err) return;
+const copyDirectory = async () => {
+  try {
+    const result = await readdir(path.join(__dirname, 'files'), 'utf-8');
+    const thereFolderFilesCopy = (await readdir(path.join(__dirname), 'utf-8')).includes('files-copy');
+    if (thereFolderFilesCopy) await rm(path.join(__dirname, 'files-copy'), { recursive: true });
+    await mkdir(path.join(__dirname, 'files-copy'));
+    await result.forEach(file => {
+      copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
     });
-    
-    files.forEach(file => {
-      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
-        if (err) throw err;
-      });
-    });
-  });
-});
+  } catch (err) {
+    throw new err;
+  }
+};
+copyDirectory();
